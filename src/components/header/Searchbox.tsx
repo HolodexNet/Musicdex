dcb16928a55b112ecc83af544a23996b06f0d1e4
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2021-12-19 23:13:00 -0800

    fancy searchbox

--- src/components/header/Searchbox.tsx
+++ src/components/header/Searchbox.tsx
@@ -1,23 +1,41 @@
 import {
   Box,
+  HStack,
   IconButton,
   Input,
   InputGroup,
   InputRightElement,
 } from "@chakra-ui/react";
 import { jsx } from "@emotion/react";
-import { FiSearch } from "react-icons/fi";
+import { useState } from "react";
+import { FiFilter, FiSearch } from "react-icons/fi";
 
 export const Searchbox: React.FC = () => {
+  let [isFocused, setFocused] = useState(false);
   return (
     <Box w={"40%"}>
       <InputGroup>
         <Input
           variant="filled"
           placeholder="Search for VTubers, songs, original artists..."
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
         />
-        <InputRightElement>
-          <IconButton aria-label="Search" icon={<FiSearch />}></IconButton>
+        <InputRightElement width="80px">
+          <HStack>
+            <IconButton
+              color="green.400"
+              bgColor={isFocused ? "green.100" : "whiteAlpha.200"}
+              size="sm"
+              aria-label="Search"
+              icon={<FiSearch />}
+            ></IconButton>
+            <IconButton
+              size="sm"
+              aria-label="Advanced Search"
+              icon={<FiFilter />}
+            ></IconButton>
+          </HStack>
         </InputRightElement>
       </InputGroup>
     </Box>
