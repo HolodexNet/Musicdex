8a1192e4e840f7a5fa5a733dde4429e0b0c15db8
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-12-28 01:27:37 -0800

    Add dev:local-api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,28 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const { env } = require("process");
 
-module.exports = function (app) {
-  app.use(
-    "/api",
+const targets = {
+  staging: () =>
     createProxyMiddleware({
       target: "https://staging.holodex.net/",
       changeOrigin: true,
-      // pathRewrite: {
-      //   "^/api/v2": "/v2",
-      // },
-    })
-  );
+    }),
+  local: () =>
+    createProxyMiddleware({
+      target: "http://localhost:2434/",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api/v2": "/v2",
+      },
+    }),
+};
+module.exports = function (app) {
+  let getMiddleware = targets.staging;
+
+  if (env.API_TARGET && targets[env.API_TARGET]) {
+    getMiddleware = targets[env.API_TARGET];
+    console.info("Proxying API to ", env.API_TARGET);
+  }
+
+  app.use("/api", getMiddleware());
 };
