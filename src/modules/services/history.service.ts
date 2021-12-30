ca4b1c786eeb81b2a9ecd840007c5bf54c9b8758
David Chen <3145205+RiceCakess@users.noreply.github.com>
2021-12-30 02:28:09 -0800

    Update history to be a playlist

--- src/modules/services/history.service.ts
+++ src/modules/services/history.service.ts
@@ -3,16 +3,16 @@ import { useClient } from "../client";
 import { DEFAULT_FETCH_CONFIG } from "./defaults";
 
 export const useHistory = (
-  config: UseQueryOptions<Song[], unknown, Song[], [string]> = {}
+  config: UseQueryOptions<PlaylistFull, unknown, PlaylistFull, [string]> = {}
 ) => {
   const { AxiosInstance } = useClient();
 
   const result = useQuery(
     ["history"],
-    async (q): Promise<Song[]> => {
+    async (q): Promise<PlaylistFull> => {
       // TODO: maybe add if-modified caching?
       // TODO: maybe change to playlist resource?
-      return (await AxiosInstance<Song[]>(`/musicdex/history`)).data;
+      return (await AxiosInstance<PlaylistFull>(`/musicdex/history`)).data;
     },
     { ...DEFAULT_FETCH_CONFIG, ...config }
   );
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,20 +1,57 @@
-import { Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Heading, Stack, useColorModeValue } from "@chakra-ui/react";
 import { QueryStatus } from "../components/common/QueryStatus";
 import { SongTable } from "../components/data/SongTable";
 import { PageContainer } from "../components/layout/PageContainer";
+import { PlaylistButtonArray } from "../components/playlist/PlaylistButtonArray";
+import { PlaylistHeading } from "../components/playlist/PlaylistHeading";
 import { useHistory } from "../modules/services/history.service";
+import { useStoreActions } from "../store";
 
 export function History() {
   // const history = useStoreState((store) => store.playback.history);
-  const { data: history, ...status } = useHistory();
+  const { data: playlist, ...status } = useHistory();
+  const bgColor = useColorModeValue("bgAlpha.50", "bgAlpha.900");
+  // const {description, }
+  const queueSongs = useStoreActions((actions) => actions.playback.queueSongs);
+  const setPlaylist = useStoreActions(
+    (actions) => actions.playback.setPlaylist
+  );
 
   return (
     <PageContainer>
-      <Stack spacing={4}>
-        <Heading>Recently Played</Heading>
-        <QueryStatus queryStatus={status} />
-        {history?.length && <SongTable songs={history}></SongTable>}
-      </Stack>
+      <Box
+        bgColor={bgColor}
+        position="relative"
+        mt="12"
+        p={{ base: 2, xl: 4 }}
+        borderRadius={5}
+      >
+        <PlaylistHeading
+          title={playlist?.title || "Recently Played"}
+          description={playlist?.description || "Your recently played songs"}
+          canEdit={false}
+          editMode={false}
+          count={playlist?.content?.length || ""}
+        />
+        {playlist?.content && (
+          <PlaylistButtonArray
+            playlist={playlist}
+            canEdit={false}
+            editMode={false}
+            onPlayClick={() => {
+              setPlaylist({ playlist });
+            }}
+            onAddQueueClick={() => {
+              playlist.content &&
+                queueSongs({
+                  songs: [...playlist.content],
+                  immediatelyPlay: false,
+                });
+            }}
+          />
+        )}
+        {playlist?.content && <SongTable songs={playlist.content} />}
+      </Box>
     </PageContainer>
   );
 }
