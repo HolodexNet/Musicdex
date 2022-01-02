c9cfce22d8a22c36171e985c42584cf15cee29a6
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-02 02:21:30 -0800

    deepen alpha value for bgAlpha

--- src/theme.ts
+++ src/theme.ts
@@ -5,7 +5,7 @@ const localColors = {
   brand: generatePalette("rgba(82, 152, 182, 1)"),
   n2: generatePalette("#F772B0"),
   bg: generatePalette("rgba(98, 98, 114, 1)", { originalAtMidpoint: true }),
-  bgAlpha: generatePalette("rgba(98, 98, 114, 0.5)"),
+  bgAlpha: generatePalette("rgba(98, 98, 114, 0.7)"),
 };
 
 console.log(localColors);
