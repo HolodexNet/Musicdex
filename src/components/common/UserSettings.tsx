0cf96ea57aa676ff59513f0e6da1e2aada1ac81e
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-12 00:52:52 -0800

    user settings Login awareness

--- src/components/common/UserSettings.tsx
+++ src/components/common/UserSettings.tsx
@@ -14,35 +14,39 @@ import {
   HStack,
   Box,
 } from "@chakra-ui/react";
+import { useClient } from "../../modules/client";
 import { LoginButtons } from "../header/Login";
 
 export function UserSettings() {
+  const { isLoggedIn } = useClient();
   return (
     <Stack w="full" direction="row" alignItems="stretch">
-      <Stack spacing={4} align={"center"} maxW={"md"} w={"full"} py={8}>
-        <FormControl id="userName" isRequired>
-          <FormLabel>Change Username</FormLabel>
-          <Input
-            placeholder="UserName"
-            _placeholder={{ color: "gray.500" }}
-            type="text"
-          />
-        </FormControl>
-        <Stack spacing={6} direction={["column", "row"]}>
-          <Button
-            bg={"blue.400"}
-            color={"white"}
-            w="full"
-            _hover={{
-              bg: "blue.500",
-            }}
-          >
-            Submit
-          </Button>
+      {isLoggedIn && (
+        <Stack spacing={4} align={"center"} maxW={"md"} w={"full"} py={8}>
+          <FormControl id="userName" isRequired>
+            <FormLabel>Change Username</FormLabel>
+            <Input
+              placeholder="UserName"
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+            />
+          </FormControl>
+          <Stack spacing={6} direction={["column", "row"]}>
+            <Button
+              bg={"blue.400"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "blue.500",
+              }}
+            >
+              Submit
+            </Button>
+          </Stack>
         </Stack>
-      </Stack>
+      )}
       <Box flex={1}>
-        <LoginButtons isNew={false}></LoginButtons>
+        <LoginButtons isNew={!isLoggedIn}></LoginButtons>
       </Box>
     </Stack>
   );
