1c97e898be0080488fb25e78c0167a7901be4dad
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-14 21:22:54 -0800

    remove junk code

--- src/utils/SongHelper.ts
+++ src/utils/SongHelper.ts
@@ -12,17 +12,6 @@ export function formatSeconds(secs: number) {
   }:${String(seconds).padStart(2, "0") || "00"}`;
 }
 
-// const SYSTEM_TZ = Intl.DateTimeFormat().resolvedOptions().timeZone
-// export function AbsoluteTimestampForTitle(ts: Date, locale: Locale) {
-//   const ts1 = formatInTimeZone(ts, SYSTEM_TZ, 'Pp zzz', { locale });
-//   const ts2 = formatInTimeZone(ts, 'Asia/Tokyo', 'Pp zzz', { locale, });
-//   if (ts1 === ts2) {
-//     return ts1;
-//   }
-//   return `${ts1}\n${ts2}`;
-
-// }
-
 export function getVideoThumbnails(ytVideoKey: string, useWebP = false) {
   const base = useWebP
     ? "https://i.ytimg.com/vi_webp"
