97cd2923ad7c618439eebc0acdd342511bdc56cc
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-17 23:41:35 -0800

    sure add jpg too

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -58,14 +58,15 @@ registerRoute(
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith(".png"),
+    url.origin === self.location.origin &&
+    (url.pathname.endsWith(".png") || url.pathname.endsWith(".jpg")),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: "images",
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({ maxEntries: 300 }),
     ],
   })
 );
