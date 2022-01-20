fa9cbb8efab3c31cb73f000f3c7570ba6f1fbca5
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-19 17:44:01 -0800

    recalibrated defaults

--- src/modules/services/defaults.ts
+++ src/modules/services/defaults.ts
@@ -1,8 +1,9 @@
 export const DEFAULT_FETCH_CONFIG /*: Omit<UseQueryOptions, 'queryKey'>*/ = {
   refetchOnWindowFocus: false,
+  refetchOnMount: false,
   retry: 1,
   retryDelay: 200,
-  cacheTime: 1000 * 60 * 30, //30 minute cache.
+  cacheTime: 1000 * 60 * 60, //60 min cache.
   keepPreviousData: true,
   staleTime: 1000 * 60 * 5, // 5 minute staleness.
 };
