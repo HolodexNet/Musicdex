d90a9948cfad90beadd38d97691441a31e00b0d7
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-01-19 17:49:36 -0800

    use service worker caching properly

--- src/modules/channel/utils.ts
+++ src/modules/channel/utils.ts
@@ -1,4 +1,4 @@
 export function getChannelPhoto(channelId: string, size = 100) {
   const nearest = Math.min(Math.max(Math.ceil(size / 50) * 50, 50), 150);
-  return `https://holodex.net/statics/channelImg/${channelId}/${nearest}.png`;
+  return `/statics/channelImg/${channelId}/${nearest}.png`;
 }
