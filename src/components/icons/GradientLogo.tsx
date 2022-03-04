0c3b378b8155d12367d60aff8af2d197adf151e2
sphinxrave <62570796+sphinxrave@users.noreply.github.com>
2022-03-03 23:20:18 -0800

    icon ID uniquely

--- src/components/icons/GradientLogo.tsx
+++ src/components/icons/GradientLogo.tsx
@@ -1,19 +1,24 @@
 import { Icon, IconProps } from "@chakra-ui/react";
+import uniqueId from "lodash/uniqueId";
+import { useRef } from "react";
 
 export function GradientLogo(props: IconProps) {
+  const { current: idx } = useRef(uniqueId("iid-"));
   return (
     <Icon {...props}>
-      <path
-        d="M4.5 2a2 2 0 0 1 3-1.7L23 9.6a2 2 0 0 1 0 3.5L7.6 22.4a2 2 0 0 1-3-1.7V2Z"
-        fill="url(#a)"
-      />
-      <path
-        d="M0 2A2 2 0 0 1 3 .3l15.5 9.3a2 2 0 0 1 0 3.5L3.1 22.4A2 2 0 0 1 0 20.7V2Z"
-        fill="url(#b)"
-      />
+      <g>
+        <path
+          d="M4.5 2a2 2 0 0 1 3-1.7L23 9.6a2 2 0 0 1 0 3.5L7.6 22.4a2 2 0 0 1-3-1.7V2Z"
+          fill={`url(#a-${idx})`}
+        />
+        <path
+          d="M0 2A2 2 0 0 1 3 .3l15.5 9.3a2 2 0 0 1 0 3.5L3.1 22.4A2 2 0 0 1 0 20.7V2Z"
+          fill={`url(#b-${idx})`}
+        />
+      </g>
       <defs>
         <linearGradient
-          id="a"
+          id={"a-" + idx}
           x1=".2"
           y1="2.8"
           x2="19.3"
@@ -24,7 +29,7 @@ export function GradientLogo(props: IconProps) {
           <stop offset="1" stopColor="#FF3A81" />
         </linearGradient>
         <linearGradient
-          id="b"
+          id={"b-" + idx}
           x1="-4.4"
           y1="2.8"
           x2="19.5"
