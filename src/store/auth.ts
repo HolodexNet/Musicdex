2cb801a3fc2bda7105cf3a06dabf123f769edaab
David Chen <3145205+RiceCakess@users.noreply.github.com>
2022-03-23 22:57:55 -0700

    Fix cookie token auto login

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -3,22 +3,30 @@ import { User } from "../modules/client";
 
 export interface AuthModel {
   token: string | null;
+  // Store last cookie token to not reattempt
+  lastCookieToken: string | null;
   user: User | null;
 
   isLoggedIn: Computed<AuthModel, boolean>;
 
   setToken: Action<AuthModel, string | null>;
+  setLastCookieToken: Action<AuthModel, string | null>;
   setUser: Action<AuthModel, User | null>;
 }
 
 const authModel: AuthModel = {
   token: null,
   user: null,
+  lastCookieToken: null,
 
   isLoggedIn: computed((state) => state.user != null),
 
   setToken: action((state, payload) => {
     state.token = payload;
+  }),
+
+  setLastCookieToken: action((state, payload) => {
+    state.lastCookieToken = payload;
   }),
   setUser: action((state, payload) => {
     state.user = payload;
