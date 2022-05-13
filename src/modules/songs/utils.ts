e4145373e11230a3e59fd255c1fa9faee1529c84
Shiaupiau <stu43005@gmail.com>
2022-05-13 20:55:32 +0800

    Fix artwork resize

--- src/modules/songs/utils.ts
+++ src/modules/songs/utils.ts
@@ -1,13 +1,15 @@
 export function resizeArtwork(artworkUrl: string, size: number = 400) {
   // https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/9c/39/27/9c392780-3f34-d322-9dde-002618154f40/source/400x400bb.jpg
+  // https://is2-ssl.mzstatic.com/image/thumb/Music122/v4/5d/b2/7e/5db27e44-e8ac-4099-1a4e-5ff8264f14ec/VEATP-39813.jpg/100x100bb.jpg
   const adjustedSize =
     Math.round(Math.floor(window.devicePixelRatio * size) / 50) * 50;
   const match =
-    /^https:\/\/(.+?)\.mzstatic\.com\/image\/thumb\/(.+?)\/source\//.exec(
+    /^https:\/\/(.+?)\.mzstatic\.com\/image\/thumb\/(.+)\/(.+?)\//.exec(
       artworkUrl
     );
   if (!match) return artworkUrl;
   const serv = match[1];
   const thumb = match[2];
-  return `https://${serv}.mzstatic.com/image/thumb/${thumb}/source/${adjustedSize}x${adjustedSize}bb.jpg`;
+  const name = match[3];
+  return `https://${serv}.mzstatic.com/image/thumb/${thumb}/${name}/${adjustedSize}x${adjustedSize}bb.jpg`;
 }
